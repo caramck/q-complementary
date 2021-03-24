47c746ec64f39c6709edb7b6b4bb94a180eb1d11
Caroline A. McKeon <cannemckeon@gmail.com>
2021-03-24 14:57:10 -0700

    Update generateTuningScripts.py

--- generateTuningScripts.py
+++ generateTuningScripts.py
@@ -69,7 +69,7 @@ class generate_script:
                 for line in f:
                     n.write(line)
             
-            n.write("$end\n\n$rem\njobtype sp\nexchange gen\nlrc_dft 1\nomega "+str(seed_gam)+"\nbasis cc-pvtz*\nrpa true\nscf_guess sad\nscf_algorithm diis_gdm\nsymmetry false\nsym_ignore true\nmax_scf_cycles 400\n$end\n\n$xc_functional\nX HF 0.2\nX wPBE 0.8\nC PBE 1.0\n$end\n\n")
+            n.write("$end\n\n$rem\njobtype sp\nexchange gen\nlrc_dft 1\nomega "+str(seed_gam)+"\nbasis cc-pvtz\nrpa true\nscf_guess sad\nscf_algorithm diis_gdm\nsymmetry false\nsym_ignore true\nmax_scf_cycles 400\n$end\n\n$xc_functional\nX HF 0.2\nX wPBE 0.8\nC PBE 1.0\n$end\n\n")
             for g in range(0,3,1):
                 charge=charge_range[g]
                 #set up subsequent calcs
