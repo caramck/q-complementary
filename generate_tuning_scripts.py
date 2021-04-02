02c612b6ff25339c3c9625bb06398c89205e4119
Caroline A. McKeon <cannemckeon@gmail.com>
2021-04-02 13:41:51 -0700

    Adjusted for alpha beta tuning

--- generate_tuning_scripts.py
+++ generate_tuning_scripts.py
@@ -11,6 +11,7 @@ import shutil
 gamma_list=(sys.argv[1]).split(",")
 seed_gam=sys.argv[2]
 coords=sys.argv[3]
+epsilon=sys.argv[4]
 
 
 ############################################################
@@ -24,7 +25,7 @@ class generate_script:
     """
 
     #Constructor
-    def __init__(self, gamma,seed_gam,coords):
+    def __init__(self, gamma,seed_gam,coords,epsilon):
         """
         generate_scripts constructor 
 
@@ -39,6 +40,7 @@ class generate_script:
         self.gamma=gamma
         self.seed_gam=seed_gam
         self.coords=coords
+        self.epsilon=epsilon
         
         #designate new vars
         file_name=str("gamma"+str(gamma)+".in")
@@ -60,6 +62,11 @@ class generate_script:
         coords=self.coords
         seed_gam=self.seed_gam
         file_name=self.file_name
+        epsilon=self.epsilon
+
+        #define beta off epsilon
+        alpha=float(0.2)
+        beta=(float(1/epsilon))-alpha
 
         #create new text file for this run
         with open(file_name,"w+") as n:
@@ -69,11 +76,11 @@ class generate_script:
                 for line in f:
                     n.write(line)
             
-            n.write("$end\n\n$rem\njobtype sp\nexchange gen\nlrc_dft 1\nomega "+str(seed_gam)+"\nbasis cc-pvtz\nrpa true\nscf_guess sad\nscf_algorithm diis_gdm\nsymmetry false\nsym_ignore true\nmax_scf_cycles 400\n$end\n\n$xc_functional\nX HF 0.2\nX wPBE 0.8\nC PBE 1.0\n$end\n\n")
+            n.write("$end\n\n$rem\njobtype sp\nexchange gen\nlrc_dft 1\nomega "+str(seed_gam)+"\nbasis cc-pvtz\nrpa true\nscf_guess sad\nscf_algorithm diis_gdm\nsymmetry false\nsym_ignore true\nmax_scf_cycles 400\n$end\n\n$xc_functional\nX HF "+str(alpha)+"\nX wPBE "+str(beta)+"\nC PBE 1.0\n$end\n\n")
             for g in range(0,3,1):
                 charge=charge_range[g]
                 #set up subsequent calcs
-                n.write("@@@\n\n$molecule\n"+charge+"\nread\n$end\n\n$rem\njobtype sp\nexchange gen\nlrc_dft 1\nomega "+str(gamma)+"\nbasis cc-pvtz\nrpa true\nscf_guess read\nscf_algorithm diis_gdm\nsymmetry false\nsym_ignore true\nmax_scf_cycles 400\n$end\n\n$xc_functional\nX HF 0.2\nX wPBE 0.8\nC PBE 1.0\n$end\n\n")
+                n.write("@@@\n\n$molecule\n"+charge+"\nread\n$end\n\n$rem\njobtype sp\nexchange gen\nlrc_dft 1\nomega "+str(gamma)+"\nbasis cc-pvtz\nrpa true\nscf_guess read\nscf_algorithm diis_gdm\nsymmetry false\nsym_ignore true\nmax_scf_cycles 400\n$end\n\n$xc_functional\nX HF "+str(alpha)+"\nX wPBE "+str(beta)+"\nC PBE 1.0\n$end\n\n")
 
         return None
 
--- process_tuning_scripts.py
+++ process_tuning_scripts.py
@@ -1,8 +1,16 @@
+#%%
 #generate_tuning_scripts.py
 #Purpose: In a directory, generate some given number of scripts optimizing a range of gamma from a given seed value with given coordinates
 import os,sys
 import subprocess
 import shutil
+import wheel
+import numpy as np
+import xlrd
+import openpyxl
+import matplotlib as plt
+import seaborn as sns
+import numpy as np   
 
 
 ##########################################################
@@ -17,7 +25,7 @@ class file_parser:
     """
 
     #Constructor
-    def __init__(self,file_name):
+    def __init__(self,dir_name):
         """
         file_parser constructor 
 
@@ -26,107 +34,190 @@ class file_parser:
         :return none
         """
 
-        #call parse file function
-        self.file_name=file_name
+        #static attributes
+        self.dir_name=dir_name
 
-    def find_files(self)
+
+    def search_all_files(self):
         """
-        find_files: 
+        search_all_files: searches all files and saves output
 
-        :param self
         :return none
         """
 
-    #TODO edit this and make it into a clean function
-    def parse(self)
+        #TODO edit this and make it into a clean function
+        def parse_file(file_name):
+
+            """
+            parse_file: searches through a particle file for values of interest and saves
+
+            :return none
+            """
+            #Load output file
+            p=open(file_name,"r")
+            all_lines=p.readlines()
+            p.close()
+            #SCF value array
+            SCFe=[]
+            #HOMO value array
+            HOMOe=[]
+
+            s=open(file_name,"r")
+
+            # This iterating process could be more efficient - put more thought into this.
+            # Create anchors for each section
+            # Parse output file by job section: job 2 is neutral, job 3 is cation, job 4 is anion
+            sec_anchors=[]
+            cur_line=1
+            #running job tally
+            jobs=0
+
+            for line in s:
+                #keep counter
+                cur_line+=1
+                #parse output file for job sections for all three jobs of interest
+                if line.find("Running Job") !=-1:
+                    jobs+=1
+                    sec_anchors.append(cur_line)
+            s.close()
 
 
-        """
-        parse
+            #Total lines in output file
+            sec_anchors.append(len(all_lines))
 
-        :param self
-        :return none
-        """
+            cur_line=1
+            #Pull out SCF and HOMO energies for each calculation of interest
+            for i in range(jobs):
+                #Look selectively in the range in file corresponding to that calculation
 
-        file_name=self.file_name
+                #initiate homoArray outside of loop
+                homoArray=[]
 
-        #Load output file
-        p=open(file_name,"r")
-        allLines=p.readlines()
-        p.close()
-        #SCF value array
-        SCFe=[]
-        #HOMO value array
-        HOMOe=[]
-        s=open(fileName,"r")
+                for j in range(sec_anchors[i],sec_anchors[i+1]):
+                    cur_line+=1
+                    #search for SCF energy for that calculation
+                    if all_lines[j].find("$molecule") != -1:
+                        charge=str(all_lines[j+1])
 
-        # This iterating process could be more efficient - put more thought into this.
-        # Create anchors for each section
-        # Parse output file by job section: job 2 is neutral, job 3 is cation, job 4 is anion
-        sectionAnchors=[]
-        currentLine=1
+                    if all_lines[j].find("Total energy in the final basis set") != -1:
+                        scf_line=str(all_lines[j])
+                        split=scf_line.split()
+                        SCF_energy=(float(split[-1]))
 
-        for line in s:
-            #keep counter
-            currentLine+=1
-            #parse output file for job sections for all three jobs of interest
-            if line.find("Running Job") !=-1:
-                sectionAnchors.append(currentLine)
+                    # search for HOMO energy for that calculation
+
+                    if all_lines[j].find("-- Virtual --") != -1:
+                        HOMOline=str(all_lines[j-1])
+                        splitB=HOMOline.split()
+                        homoArray.append(float(splitB[-1]))
+
+                    #pull the corresponding gamma for this calculation
+                    if all_lines[j].find("omega") != -1:
+                        gamma_line=str(all_lines[j])
+                        split=gamma_line.split()
+                        gamma=(float(split[-1]))
     
-        s.close()
-        #Total lines in output file
-        sectionAnchors.append(len(allLines))
 
-        currentLine=1
-        #Pull out SCF and HOMO energies for each calculation of interest
-        for i in range(2,5):
-            #Look selectively in the range in file corresponding to that calculation
 
-            #initiate homoArray outside of loop
-            homoArray=[]
 
-            for j in range(sectionAnchors[i-1],sectionAnchors[i]):
-                currentLine+=1
-                #search for SCF energy for that calculation
-                if allLines[j].find("Total energy in the final basis set") != -1:
-                    SCFline=str(allLines[j])
-                    split=SCFline.split()
-                    SCFe.append(float(split[-1]))
-
-                # search for HOMO energy for that calculation
-
-                if allLines[j].find("-- Virtual --") != -1:
-                    HOMOline=str(allLines[j-1])
-                    splitB=HOMOline.split()
-                    homoArray.append(float(splitB[-1]))
-
-            # Compare the saved HOMO energies, pick the highest if there are 2.
-            if len(homoArray)==2:
-                if homoArray[0]>homoArray[1]:
-                    HOMOe.append(homoArray[0])
+                # Compare the saved HOMO energies, pick the highest if there are 2.
+                if len(homoArray)==2:
+                    if homoArray[0]>homoArray[1]:
+                        HOMO_energy=(homoArray[0])
+                    else:
+                        HOMO_energy=(homoArray[1])
                 else:
-                    HOMOe.append(homoArray[1])
-            else:
-                HOMOe.append(homoArray[0]) 
+                    HOMO_energy=(homoArray[0]) 
 
-        s.close()
 
-        resultsAr=[SCFe[0],SCFe[1],SCFe[2],HOMOe[0],HOMOe[1]]    
+                if "0 1" in str(charge):
+                    job_type=0
+                    neutral_scf=SCF_energy
+                    neutral_homo=HOMO_energy
+                elif "-1 2" in str(charge):
+                    job_type=1
+                    anion_scf=SCF_energy
+                    anion_homo=HOMO_energy
+                elif "1 2" in str(charge):
+                    job_type=2
+                    cation_scf=SCF_energy
+
+            s.close()
+
+            #add results to array
+            results_arr=[file_name,gamma,neutral_scf,anion_scf,cation_scf,neutral_homo,anion_homo]
+
+            return results_arr
+
+        def calc_homo_ip(homo,cation_scf,neutral_scf):
+            """
+            calc_homo_ip: find difference between given homo and ip values
+
+            :return none
+            """
+            ip=cation_scf-neutral_scf
+            homo_ip=np.absolute(homo+ip)
+
+            return homo_ip
+        
+        def calc_lumo_ea(lumo,neutral_scf,anion_scf):
+            """
+            calc_lumo_ea: find difference between given lumo and ea values
+
+            :return none
+            """
+            ea=neutral_scf-anion_scf
+            lumo_ea=np.absolute(lumo+ea)
+
+            return lumo_ea
+
+
+        #create dataframe headers
+        df_array=[['file','gamma','scf_neutral_ha','scf_anion_ha','scf_cation_ha','homo_neutral_ha','homo_anion_ha']]
+
+        #set file extension
+        file_string=".out"
+
+        #search all files in folder for this string, if found, include full file name in array
+        for roots, dirs, files in os.walk(self.dir_name):
+            for file_name in files:
+                if file_string in file_name:
+                    results=parse_file(self.dir_name+"/"+file_name)
+                    df_array.append(results)
+
+        #create dataframe
+        df=pd.DataFrame(data=df_array)
+        df.rename(columns=df.iloc[0])
+        df.columns=df.iloc[0]
+        
+        #add homo +ip and lumo+ea columns to df
+        #df["homo_ip"] = ((df["homo_neutral_ha"])+(df["scf_cation_ha"]-df["scf_neutral_ha"]))
+        #df["homo_ip"] = (calc_homo_ip(df["homo_neutral_ha"],df["scf_cation_ha"],df["scf_neutral_ha"]))
+
+        #print dataframe
+        print(df)
+
+        #create excel document
+        df.to_excel("/Users/CAMcKeon/Desktop/outs/tuning.xlsx")              
+
+        #create plot and save it
+        #sns.lineplot(data=df, x="gamma", y="passengers")
 
 ##########################################################
-# Parse each appropriate file                            #
+# Parse files in directory                               #
 ##########################################################
 
-#print header of table
+#find current working directory
+cwd=os.getcwd()
 
-#All files with ".out" should be parsed
-#for each .out file make a file_parser class
-#print gamma and results of array into a table
+#create file parser object
+parser=file_parser("/Users/CAMcKeon/Desktop/outs")
 
-
-#put in a dataframe
-
-#create a pyplot of homo+ip and lumo+ea and save
+#parse files in directory
+parser.search_all_files()
 
 
 
+
+
+# %%
