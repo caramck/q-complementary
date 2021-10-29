4064b4ba188badd644366459fdb78953353a4930
Caroline A. McKeon <mck@berkeley.edu>
2021-10-29 11:02:14 -0700

    Create tddft_pipeline.py

---
+++ tddft_pipeline.py
@@ -0,0 +1,8 @@
+#tddft_pipeline.py
+#Caroline McKeon
+#October 2021
+
+#Purpose: Create scripts from many thiel's set structures and perform tddft calcualtions on them.
+
+#Object: molecule in thiel's set. Each molecule has a series of associated geometries, and a directory.
+
